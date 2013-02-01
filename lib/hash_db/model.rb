a00847c5b6a0bcc5e9daf1771dca9356a99a9cf2
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-02-01 18:03:54 +0530

    Allow declaring attributes on a model using Model.keys

--- lib/hash_db/model.rb
+++ lib/hash_db/model.rb
@@ -1,4 +1,26 @@
 module HashDB
   module Model
+    def self.included(klass)
+      klass.extend ClassMethods
+    end
+
+    def initialize
+      @attributes = {}
+    end
+
+    module ClassMethods
+
+      def keys(*keys)
+        keys.each do |key|
+          define_method "#{key}" do
+            @attributes[key]
+          end
+
+          define_method "#{key}=" do |value|
+            @attributes[key] = value
+          end
+        end
+      end
+    end
   end
 end
--- spec/hash_db/model_spec.rb
+++ spec/hash_db/model_spec.rb
@@ -1,10 +1,29 @@
 require "spec_helper"
 
 describe HashDB::Model do
+  let(:model) { Class.new { include HashDB::Model } }
+
   context "include" do
-    let(:model) { Class.new { include HashDB::Model } }
     it "should be includable" do
       model.ancestors.should include HashDB::Model
+    end
+  end
+
+  context ".keys" do
+    it "should allow assigning and retrieving attributes" do
+      model.keys :integer, :string, :float, :object
+
+      m1 = model.new
+
+      m1.integer = 2
+      m1.string = "Hello"
+      m1.float = 4.2
+      m1.object = m1
+
+      m1.integer.should eq 2
+      m1.string.should eq "Hello"
+      m1.float.should eq 4.2
+      m1.object.should eq m1
     end
   end
 end
