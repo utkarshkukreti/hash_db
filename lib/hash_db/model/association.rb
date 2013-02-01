5fb21c1b458dad4241c000c59ce0b8359b3df535
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-02-02 02:06:41 +0530

    Add support for basic belongs_to associations. Again, bugs.

--- lib/hash_db/model/association.rb
+++ lib/hash_db/model/association.rb
@@ -25,6 +25,27 @@ module HashDB
           end
         end
       end
+
+      def belongs_to(name, args = {})
+        klass = args[:class]
+        key = args[:key]
+
+        define_method "#{name}" do
+          @attributes[name]
+        end
+
+        define_method "#{name}=" do |object|
+          @attributes[name] = object
+        end
+
+        define_method "#{key}" do
+          @attributes[name] && @attributes[name].id
+        end
+
+        define_method "#{key}=" do |id|
+          @attributes[name] = klass.find_by(id: id)
+        end
+      end
     end
   end
 end
--- spec/hash_db/model/association_spec.rb
+++ spec/hash_db/model/association_spec.rb
@@ -23,4 +23,29 @@ describe HashDB::Model do
       p1.user_id.should eq u2.id
     end
   end
+
+  context ".belongs_to" do
+    it "should allow assigning and retrieving objects" do
+      user = Class.new do
+        include HashDB::Model
+      end
+
+      post = Class.new do
+        include HashDB::Model
+        belongs_to :user, key: :user_id, class: user
+      end
+
+      u1 = user.create
+      u2 = user.create
+      p1 = post.create
+      p2 = post.create
+
+      p1.user = u1
+      p1.user_id.should eq u1.id
+      p1.user = u2
+      p1.user_id.should eq u2.id
+      p1.user_id = u1.id
+      p1.user.should eq u1
+    end
+  end
 end
