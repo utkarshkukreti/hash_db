dded170281bb4acea509608f3c8bfda4cbcef846
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2013-02-02 01:46:30 +0530

    Better specs for has_many associations.

--- spec/hash_db/model/association_spec.rb
+++ spec/hash_db/model/association_spec.rb
@@ -13,13 +13,14 @@ describe HashDB::Model do
       end
 
       u1 = user.create
+      u2 = user.create
       p1 = post.create
-      u1.posts << p1
-      p2 = u1.posts.create
+      p2 = post.create
 
-      u1.posts.size.should eq 2
+      u1.posts << p1
       p1.user_id.should eq u1.id
-      p2.user_id.should eq u1.id
+      u2.posts << p1
+      p1.user_id.should eq u2.id
     end
   end
 end
